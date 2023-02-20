864ef1b4ced69f989c576e47c1a5dc2d897835b2
Mawidev <savio.riccardo95@gmail.com>
2023-02-20 22:50:14 +0100

    digital clock

--- src/components/DigitalClock.jsx
+++ src/components/DigitalClock.jsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
 import ReturnButton from "./ReturnButton";
 
 export default function DigitalClock () {
+    const clock = new Date()
+    let [time,setTime] = useState()
+    const [wait,setWait] = useState(true)
+    setTimeout(() => {
+        setTime(time=clock.toLocaleTimeString())
+        setWait(false)
+    }, 1000);
     return(
         <>
             <h1>Digital Clock</h1>
+
+            <div style={
+                {
+                    border:'2px solid white',
+                    borderRadius:'10px',
+                    paddingLeft:'25px',
+                    paddingRight:'25px'
+                }
+            }>
+                <h1>{wait ? clock.toLocaleTimeString() : time}</h1>
+            </div>
+
             <ReturnButton/>
         </>
     )
