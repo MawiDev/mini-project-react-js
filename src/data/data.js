2831f21470e9f9053390269cf6c62ed29ba77478
Mawidev <savio.riccardo95@gmail.com>
2023-02-20 16:21:04 +0100

    useState and map()

---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+export const teams = [
+    {
+        id: 0,
+        name:'team1',
+        points: 10
+    },
+    {
+        id: 2,
+        name:'team2',
+        points: 1
+    },
+    {
+        id: 3,
+        name:'team3',
+        points: 5
+    },
+    {
+        id: 4,
+        name:'team5',
+        points: 0
+    },
+]
